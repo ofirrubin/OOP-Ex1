098d315349c17bc61720eac01e13a4fce50f5826
Ofir Rubin <ofirrubin02@gmail.com>
2021-11-17 15:36:31 +0200

    Updated into classes

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import argparse
 import pandas as pd
 import json
 import os
+from sortedcontainers import SortedSet
+
+basePath = r"/Users/ofirrubin/OOP_2021/Assignments/Ex1/data/Ex1_input/"
+OUTPUT_PATH = os.path.join(basePath, r"Ex1_Calls", r"out.csv")
+BUILDING_PATH = os.path.join(basePath, r"Ex1_Buildings", r"B5.json")
+CALLS_PATH = os.path.join(basePath, r"Ex1_Calls", r"Calls_d.csv")
 
 
 class Elevator:
@@ -37,42 +44,68 @@ class Building:
                + self.elevators.__repr__() + ")"
 
 
-def get_df(src_path: str, out_path: str):
-    if os.path.isfile(out_path):
-        os.remove(out_path)
-    with open(out_path, "a+") as out:
-        out.write("elevatorCall,time,src,dest,status,ele\n")  # To make it easier.
-        with open(src_path, "r") as src:
-            data = src.read()
-        out.write(data)
-    df = pd.read_csv(out_path, usecols=["time", "src", "dest", "ele"])
-    return df
+class Lift:
+    def __init__(self, ele: Elevator):
+        self.ele = ele
+        self.in_way = SortedSet()
+        self.an_way = SortedSet()
+
+    # def add_call(self, call: Call):
 
 
-def building(path: str):
-    with open(path, "r") as j_file:
-        j = json.load(j_file)
-        return Building(**j)
+class LiftAlgo:
+    def __init__(self, building_path, calls_path, out_path):
+        self.paths = {"building": building_path, "calls": calls_path, "out": out_path}
+        self.b = None
+        self.df = None
+
+    def __set_building(self):
+        with open(self.paths['building'], "r") as j_file:
+            j = json.load(j_file)
+            self.b = Building(**j)
+
+    def __set_dataframe(self):
+        if os.path.isfile(self.paths['out']):
+            os.remove(self.paths['out'])
+        with open(self.paths['out'], "a+") as out:
+            out.write("elevatorCall,time,src,dest,status,ele\n")  # To make it easier.
+            with open(self.paths['calls'], "r") as src:
+                data = src.read()
+            out.write(data)
+        self.df = pd.read_csv(self.paths['out'], usecols=["time", "src", "dest", "ele"])
+
+    def start(self):
+        # (re)load data from file
+        self.__set_building()
+        self.__set_dataframe()
+
+        if self.df['src'].min() < self.b.minFloor or self.df['dest'].min() < self.b.minFloor or\
+           self.df['src'].max() > self.b.maxFloor or self.df['dest'].max() > self.b.maxFloor:
+            raise ValueError("There is a call out of range")
+        pass
+
+    def export(self):
+        if self.df is None:
+            return
 
 
-def elements_parser(building_path: str, calls_path: str, output_path: str):
-    b = building(building_path)
-    df = get_df(calls_path, output_path)
-    # To fix:
-    if df['src'].min() < b.minFloor or df['dest'].min() < b.minFloor or df['src'].max() > b.maxFloor or df['dest'].max() > b.maxFloor:
-        raise ValueError("There is a call out of range")
-    return b, df
-
-
-basePath = r"/Users/ofirrubin/OOP_2021/Assignments/Ex1/data/Ex1_input/"
-OUTPUT_PATH = os.path.join(basePath, r"Ex1_Calls", r"out.csv")
-BUILDING_PATH = os.path.join(basePath, r"Ex1_Buildings", r"B5.json")
-CALLS_PATH = os.path.join(basePath, r"Ex1_Calls", r"Calls_d.csv")
-
-
-if __name__ == "__main__":
+def main():
+    parser = argparse.ArgumentParser(description="Lift Offline Algorithm")
+    parser.add_argument('-building', type=str, help="Building path")
+    parser.add_argument('-calls', type=str, help="Calls path")
+    parser.add_argument('-output', type=str, help="Output path")
+    args = parser.parse_args()
+    print(args)
+    algo = LiftAlgo(args.building, args.calls, args.output)
     try:
-        elements_parser(building_path=BUILDING_PATH, calls_path=CALLS_PATH, output_path=OUTPUT_PATH)
+        algo.start()
     except ValueError as e:
         print("An error occured: ", e)
         exit(-1)
+    except FileNotFoundError as e:
+        print("File not found: ", e)
+        exit(-1)
+
+
+if __name__ == "__main__":
+    main()
